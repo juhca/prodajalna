13287ab5b387d96ac6188994d45d916df3705898
Tomas Štefe <tomas.stefe@gmail.com>
2016-05-05 17:23:28 +0000

    Ustvarjanje računa

--- eracun.js
+++ eracun.js
@@ -133,7 +133,7 @@ var pesmiIzRacuna = function(racunId, callback) {
     Track.TrackId IN (SELECT InvoiceLine.TrackId FROM InvoiceLine, Invoice \
     WHERE InvoiceLine.InvoiceId = Invoice.InvoiceId AND Invoice.InvoiceId = " + racunId + ")",
     function(napaka, vrstice) {
-      console.log(vrstice);
+      callback(vrstice);
     })
 }
 
@@ -142,13 +142,32 @@ var strankaIzRacuna = function(racunId, callback) {
     pb.all("SELECT Customer.* FROM Customer, Invoice \
             WHERE Customer.CustomerId = Invoice.CustomerId AND Invoice.InvoiceId = " + racunId,
     function(napaka, vrstice) {
-      console.log(vrstice);
+      callback(vrstice);
     })
 }
 
 // Izpis računa v HTML predstavitvi na podlagi podatkov iz baze
 streznik.post('/izpisiRacunBaza', function(zahteva, odgovor) {
-  odgovor.end();
+  var form = new formidable.IncomingForm();
+  form.parse(zahteva, function (napaka1, polja, datoteke) {
+        // pridobim racun_id iz forme
+      var racun_id = polja.seznamRacunov;
+        // pridobi pesmi iz racuna
+      pesmiIzRacuna(racun_id, function(pesmi){
+          // pridobi stranko iz racuna
+        strankaIzRacuna(racun_id, function(stranka){
+            // ustvarim racun
+          odgovor.setHeader('content-type', 'text/xml');
+          odgovor.render('eslog', {
+          vizualiziraj: true,
+          postavkeRacuna: pesmi,
+          narocnik: stranka[0]
+          });
+        });
+      });
+    //odgovor.redirect('/')
+  });
+  //odgovor.end()
 })
 
 // Izpis računa v HTML predstavitvi ali izvorni XML obliki
